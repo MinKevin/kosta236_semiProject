5d77b5b9ce69cdebfb17adfd734d21983abc7be2
JK <alswkdrb1@naver.com>
2022-04-26 16:09:15 +0900

    SG_1 Commit Test

--- testGit/src/test/model/test.java
+++ testGit/src/test/model/test.java
@@ -3,5 +3,6 @@ package test.model;
 public class test {                                                                    
     public static void main(String[] args) {
         System.out.println("JK_1 ㅎㅎ Hello World");
+        System.out.println("hi");
     }
 }
